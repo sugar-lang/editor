03dca3dad1180a1df22db2cd83ed72829264f622
Sebastian Erdweg <seba.github@gmail.com>
2013-12-21 10:18:35 +0100

    use localclasspath

--- build.main.sh
+++ build.main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 CP=`cat $1`
-OPT_JAR_LIST=$CP ant -f build.main.xml -Dcompile.classpath=$CP shell-all
+LOCALCLASSPATH=$CP ant -f build.main.xml -Dcompile.classpath=$CP shell-all
 rm $1
 
