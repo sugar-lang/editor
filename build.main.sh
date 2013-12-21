962548367ad7f04769c1337271b8cc71d5a0c677
Sebastian Erdweg <seba.github@gmail.com>
2013-12-21 10:48:33 +0100

    explicit native prefix

--- build.main.sh
+++ build.main.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
 CP=`cat $1`
-LOCALCLASSPATH=$CP ant -f build.main.xml -Dcompile.classpath=$CP shell-all
+CP_=$(echo $CP | tr ":" "\n")
+
+for path in $CP_ ; do
+  if [[ "$path" == *"org.strategoxt.imp.nativebundle"* ]]; then
+    NATIVE_JAR=$path
+  fi
+done
+
+echo $NATIVE_JAR
+mkdir native
+(cd native && jar xf $NATIVE_JAR)
+
+LOCALCLASSPATH=$CP ant -f build.main.xml -Dcompile.classpath=$CP -Declipse.spoofaximp.nativeprefix=native/native/linux shell-all
+
+rm -rf native
 rm $1
+
 
