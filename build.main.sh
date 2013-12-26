0fdef1b23a8143b68fce981805722ab85ea4dacf
Sebastian Erdweg <seba.github@gmail.com>
2013-12-26 11:26:35 +0100

    fail on error

--- build.main.sh
+++ build.main.sh
@@ -13,9 +13,8 @@ echo $NATIVE_JAR
 mkdir native
 (cd native && jar xf $NATIVE_JAR)
 
-LOCALCLASSPATH=$CP ant -f build.main.xml -Dcompile.classpath=$CP -Declipse.spoofaximp.nativeprefix=native/native/linux shell-all
-
-rm -rf native
-rm $1
+LOCALCLASSPATH=$CP ant -f build.main.xml -Dcompile.classpath=$CP -Declipse.spoofaximp.nativeprefix=native/native/linux shell-all \
+&& rm -rf native \
+&& rm $1
 
 
