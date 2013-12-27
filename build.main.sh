b6ab3ee1f3bbeabfc9d42d2f8a460b9f768a456b
Sebastian Erdweg <seba.github@gmail.com>
2013-12-27 11:11:49 +0100

    missing dash

--- build.main.sh
+++ build.main.sh
@@ -4,10 +4,8 @@ CP=`cat $1`
 CP_=$(echo $CP | tr ":" "\n")
 
 for path in $CP_ ; do
-  echo $path
   MATCH=`echo $path | sed 's/.*org\.strategoxt\.imp\.nativebundle.*/ok/'`
   if [ "$MATCH" = "ok" ]; then
-    echo "MATCH"
     NATIVE_JAR=$path
   fi
 done
@@ -15,7 +13,7 @@ done
 mkdir native
 (cd native && jar xf $NATIVE_JAR)
 
-LOCALCLASSPATH=$CP ant -f build.main.xml -Dcompile.classpath=$CP -Declipse.spoofaximp.nativeprefix=native/native/linux shell-all \
+LOCALCLASSPATH=$CP ant -f build.main.xml -Dcompile.classpath=$CP -Declipse.spoofaximp.nativeprefix=native/native/linux/ shell-all \
 && rm -rf native \
 && rm $1
 
