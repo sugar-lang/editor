e2228b48bbdb0f3b4c5d70cc1d26da24a0cca705
Moritz Lichter <lichtemo@googlemail.com>
2014-04-12 14:45:41 +0200

    Debug output for SDF classpath helper script

--- build.main.sh
+++ build.main.sh
@@ -11,6 +11,8 @@ for path in $CP_ ; do
     NATIVE_JAR=$path
   fi
 done
+echo "Native Jar File"
+echo $NATIVE_JAR
 
 mkdir native
 (cd native && jar xf $NATIVE_JAR)
@@ -21,9 +23,12 @@ if [ "$UNAME" = "Darwin" ]; then
   OS="macosx"
 fi
 
+echo "SDF to table"
+echo "native/native/$OS/sdf2table"
+
 chmod +x native/native/$OS/sdf2table
 chmod +x native/native/$OS/implodePT
-LOCALCLASSPATH=$CP ant -f build.main.xml -Dcompile.classpath=$CP -Declipse.spoofaximp.nativeprefix=$BASEDIR/native/native/$OS/ shell-all \
-&& rm -rf native \
-&& rm $1
+LOCALCLASSPATH=$CP ant -f build.main.xml -Dcompile.classpath=$CP -Declipse.spoofaximp.nativeprefix=$BASEDIR/native/native/$OS/ shell-all ## \
+#&& rm -rf native \
+##&& rm $1
 
